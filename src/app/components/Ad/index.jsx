35c42ac8cffa490aaf7bb8a99a8c24f554faa62a
Ian deBoisblanc <ian@reddit.com>
2018-03-06 10:56:19 -0800

    Revert "Rework all ad-related logic and components to support withAds query param and impression tracking"
    This reverts commit 800897a605dc162fec1eb35a47140054bd5e0be3.

--- src/app/components/Ad/index.jsx
+++ src/app/components/Ad/index.jsx
@@ -1,43 +1,63 @@
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
-
-import Observer from '@researchgate/react-intersection-observer';
+import { createSelector } from 'reselect';
+import throttle from 'lodash/throttle';
 
 import { isHidden } from 'lib/dom';
 
 import * as adActions from 'app/actions/ads';
+import { TOP_NAV_HEIGHT } from 'app/constants';
 import Post from 'app/components/Post';
 
 const T = React.PropTypes;
 
-const IMPRESSION_THRESHOLD = 0;
-const VIEWABILITY_THRESHOLD = 0.5;
-const VIEWABILITY_TIME = 1000;
-
 class Ad extends React.Component {
   static propTypes = {
     // ownProps
-    postId: T.string.isRequired,
+    adId: T.string.isRequired,
     placementIndex: T.number.isRequired,
     postProps: T.object.isRequired,
 
     // props from connect
-    onImpression: T.func.isRequired,
-    onViewableImpression: T.func.isRequired,
+    adTracked: T.bool.isRequired,
+    onAdMadeImpression: T.func.isRequired,
     onAdblockDetected: T.func.isRequired,
   };
 
   constructor(props) {
     super(props);
-    this.state = {
-      madeImpression: false,
-      madeViewableImpression: false,
-    };
+
+    this.scrollListener = throttle(() => this.checkImpression(), 100);
   }
 
   componentDidMount() {
-    this.checkAdblock();
+    if (!this.props.adTracked) {
+      window.addEventListener('scroll', this.scrollListener);
+      this.checkImpression(); // check manually in case the ad is visible on the first render
+      this.checkAdblock();
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollListener);
+  }
+
+  checkImpression() {
+    const node = findDOMNode(this);
+    const winHeight = window.innerHeight;
+    const rect = node.getBoundingClientRect();
+    const top = rect.top;
+    const height = rect.height;
+    const bottom = top + rect.height;
+    const middle = (top + bottom) / 2;
+    const middleIsAboveBottom = middle < winHeight;
+    const middleIsBelowTop = bottom > TOP_NAV_HEIGHT + height / 2;
+
+    if (middleIsAboveBottom && middleIsBelowTop) {
+      window.removeEventListener('scroll', this.scrollListener);
+      this.props.onAdMadeImpression();
+    }
   }
 
   checkAdblock() {
@@ -45,66 +65,20 @@ class Ad extends React.Component {
       this.props.onAdblockDetected();
     }
   }
-  
-  handleImpressionChange = e => {
-    // an element must enter the viewport to register as impression
-    if (e.isIntersecting) {
-      this.onImpression();
-    }
-  }
-
-  onImpression() {
-    // we only need to track the impression once.
-    if (this.state.madeImpression) { return; }
-    this.setState({ madeImpression: true }, this.props.onImpression);
-  }
-
-  handleViewabilityChange = e => {
-    // an element must be 50% viewable for at least 1 second
-    // to be consider viewable per the IAB standard.
-    if (e.isIntersecting && e.intersectionRatio >= VIEWABILITY_THRESHOLD) {
-      this.timer = window.setTimeout(() => {
-        this.onViewableImpression();
-      }, VIEWABILITY_TIME);
-      return;
-    }
-
-    // reset if the item is no longer in view.
-    if (this.timer) {
-      clearTimeout(this.timer);
-    }
-  }
-
-  onViewableImpression() {
-    // we only need to track the impression once.
-    if (this.state.madeViewableImpression) { return; }
-    this.setState({ madeViewableImpression: true }, this.props.onViewableImpression);
-  }
 
   render() {
-    const { postProps, postId } = this.props;
-    return (
-      <Observer
-        threshold={ VIEWABILITY_THRESHOLD }
-        onChange={ this.handleViewabilityChange }
-        disabled={ this.state.madeViewableImpression }
-      >
-        <Observer
-          threshold={ IMPRESSION_THRESHOLD }
-          onChange={ this.handleImpressionChange }
-          disabled={ this.state.madeImpression }
-        >
-          <Post { ...postProps } postId={ postId } key={ `post-id-${postId}` }/>
-        </Observer>
-      </Observer>
-    );
+    return <Post { ...this.props.postProps } />;
   }
 }
 
-const mapDispatchToProps = (dispatch, { postId, placementIndex }) => ({
-  onImpression() { dispatch(adActions.trackImpression(postId)); },
-  onViewableImpression() { dispatch(adActions.trackViewableImpression(postId)); },
+const selector = createSelector(
+  (state, props) => state.adRequests[props.adId],
+  adRequest => ({ adTracked: adRequest.impressionTracked }),
+);
+
+const mapDispatchToProps = (dispatch, { adId, placementIndex }) => ({
+  onAdMadeImpression() { dispatch(adActions.track(adId)); },
   onAdblockDetected() { dispatch(adActions.trackAdHidden(placementIndex)); },
 });
 
-export default connect(undefined, mapDispatchToProps)(Ad);
+export default connect(selector, mapDispatchToProps)(Ad);
