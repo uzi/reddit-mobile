3aeb63711215bd346180a221d21671c104de064b
Andrew Boekhoff <v.andrew.boekhoff@reddit.com>
2018-06-12 21:15:20 -0700

    Make nsfw xpromo the default experience

--- src/app/components/XPromoPill/index.jsx
+++ src/app/components/XPromoPill/index.jsx
@@ -5,9 +5,6 @@ import config from 'config';
 import { getBranchLink } from 'lib/xpromoState';
 import { connect } from 'react-redux';
 import cx from 'lib/classNames';
-import { flags } from 'app/constants';
-import { getActiveExperimentVariant } from 'lib/experiments';
-import { EXPERIMENT_NAMES } from '../../selectors/xpromo';
 
 class XPromoPill extends React.Component {
   constructor() {
@@ -19,6 +16,7 @@ class XPromoPill extends React.Component {
 
   render() {
     const { dismissed } = this.state;
+    const { href } = this.props;
 
     const onClick = (e) => {
       this.setState({ dismissed: true });
@@ -27,12 +25,8 @@ class XPromoPill extends React.Component {
       return false;
     };
 
-    if (!this.props.active) {
-      return null;
-    }
-
     return (
-      <a className={ cx('XPromoPill', { dismissed }) } href={ this.props.href }>
+      <a className={ cx('XPromoPill', { dismissed }) } href={ href }>
         <span>OPEN REDDIT APP</span>
         <img
           className="XPromoPill__close"
@@ -45,23 +39,15 @@ class XPromoPill extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const { NSFW_XPROMO } = flags;
-  const active = !!getActiveExperimentVariant(state, EXPERIMENT_NAMES[NSFW_XPROMO]);
   const href = getBranchLink(state, ownProps.url, {
     channel: 'xpromo',
-    campaign: 'mweb',
-    feature: 'mweb_nsfw_xpromo',
-    id: 'treatment_1',
+    campaign: 'nsfw_xpromo',
+    feature: 'mweb',
     utm_source: 'xpromo',
     utm_medium: 'mweb',
-    utm_name: 'mweb_nsfw_xpromo',
-    utm_term: 'treatment_1',
+    utm_name: 'nsfw_xpromo',
   });
-  return { href, active };
+  return { href };
 };
 
 export default connect(mapStateToProps)(XPromoPill);
-
-/*
-https://www.reddit.com/?channel=xpromo&feature=mweb_nsfw_xpromo&campaign=mweb&%2524og_redirect=https%3A%2F%2Fwww.reddit.com%2Fr%2Favocadosgonewild%2F&%2524deeplink_path=%2Fr%2Favocadosgonewild%2F&%2524android_deeplink_path=reddit%2Fr%2Favocadosgonewild%2F&mweb_loid=00000000001idl174i&mweb_loid_created=1528158071812&mweb_user_id36=&mweb_user_name=&domain=localhost%3A4444&geoip_country=US&user_agent=Mozilla%2F5.0+(iPhone%3B+CPU+iPhone+OS+11_0+like+Mac+OS+X)+AppleWebKit%2F604.1.38+(KHTML%2C+like+Gecko)+Version%2F11.0+Mobile%2F15A372+Safari%2F604.1&base_url=%2Fr%2Favocadosgonewild&referrer_domain=&referrer_url=&language=&dnt=false&compact_view=true&adblock=false&session_id=Ob36ZwPYy0gN5qnwLa&loid=00000000001idl174i&loid_created=1528158071812&reddaid=&sr_id=4840364&sr_name=avocadosgonewild&id=treatment_1&tags%5B%5D=mweb_xpromo_ad_loading_ios&tags%5B%5D=mweb_xpromo_ad_loading_ios_variant_control_2&tags%5B%5D=mweb_xpromo_ad_loading_ios_id_186&tags%5B%5D=mweb_nsfw_xpromo&tags%5B%5D=mweb_nsfw_xpromo_variant_treatment_1&tags%5B%5D=mweb_nsfw_xpromo_id_361&tags%5B%5D=client_settings_empty&_branch_match_id=532340418190380955
-*/
